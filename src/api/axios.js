1d10b127d40c63d0504fa748c0b640bc14553edf
Betuel <betuel.arones@tecsup.edu.pe>
2026-03-25 18:29:25 -0500

    Conexión con el  backend arreglado

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://crud-users-1-g57m.onrender.com/api' 
+    baseURL: 'https://crud-users-1-g57m.onrender.com' 
 });
 
 export default api;
