5da1d0cf84a7fcfe04292af993de618b242e1d12
Betuel <betuel.arones@tecsup.edu.pe>
2026-03-25 18:59:30 -0500

    Conexión con el  backend arreglado

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const DEFAULT_PROD_URL = 'https://crud-users-1-g57m.onrender.com';
+const DEFAULT_PROD_URL = 'https://crud-users-1-g57m.onrender.com/api';
 const baseURL =
   (import.meta.env.VITE_API_BASE_URL && import.meta.env.VITE_API_BASE_URL.replace(/\/$/, '')) ||
   (import.meta.env.DEV ? 'http://localhost:3000' : DEFAULT_PROD_URL);
